bd39af5d40ff24c734b986ee95bc1bd924484840
Kaustubh Trivedi <34265148+codekaust@users.noreply.github.com>
2018-03-13 22:12:34 +0530

    initial commit

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+
+    public GameObject gameOverText;
+    public bool gameOver= false;
+    public static GameControl instance;
+
+	void Awake () {
+        if(instance==null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void BirdDied()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+}
