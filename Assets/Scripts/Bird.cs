3a7696a54a6fb472354b8c96ed7db4bf7a2bff12
Kaustubh Trivedi <34265148+codekaust@users.noreply.github.com>
2018-03-14 19:23:32 +0530

    mostly completed

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Bird : MonoBehaviour {
 
     public float upforce=150f;
-
+    
     private bool isDead = false;
     private Rigidbody2D rb2d;
     private Animator anim;
@@ -14,10 +14,13 @@ public class Bird : MonoBehaviour {
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+        rb2d.AddForce(new Vector2(0, 80f));
+        anim.SetTrigger("Flap");
+    }
+
+    // Update is called once per frame
+    void Update () {
         if (isDead == false)
         {
             if (Input.GetMouseButtonDown(0))
@@ -32,8 +35,9 @@ public class Bird : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag.Equals("Ground"))
+        if (!collision.gameObject.tag.Equals("Ceiling"))
         {
+            rb2d.velocity = Vector2.zero;
             isDead = true;
             anim.SetTrigger("Die");
 
--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -1,32 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour {
 
     public GameObject gameOverText;
-    public bool gameOver= false;
+    public bool gameOver = false;
+    private int score;
+
+    public float scrollSpeed = 1.5f;
+
     public static GameControl instance;
 
-	void Awake () {
+    public Text scoreText;
+
+    void Awake()
+    {
         if(instance==null)
         {
             instance = this;
         }
-        else if (instance != this)
+        else if(instance!=this)
         {
             Destroy(gameObject);
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    }
+
+    private void Update()
+    {
+        if (gameOver == true && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
 
     public void BirdDied()
     {
         gameOverText.SetActive(true);
         gameOver = true;
+    }
+
+    public void BirdScored()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+        score++;
+        scoreText.text = "Score :" + score.ToString();
     }
 }
